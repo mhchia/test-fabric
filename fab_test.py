5adcb6e85c2a9b70e431ca39ddc0fa298c206d63
mhchia <kevin.mh.chia@gmail.com>
2018-06-19 00:02:21 +0800

    Version working with nodes on multiple machines

--- fab_test.py
+++ fab_test.py
@@ -26,16 +26,15 @@ pubsub_path = f"{go_path}/src/github.com/libp2p/go-floodsub"
 go_executable_path = "/usr/local/go/bin/go"
 # git related for pubsub
 poc_remote = "mine"
-# poc_branch = "temp-poc-testing"
-poc_branch = "1dc628d96810945f71dcda9d304965907fb961ae"
+poc_branch = "temp-poc-testing"
+# poc_branch = "1dc628d96810945f71dcda9d304965907fb961ae"
 pubsub_remote = "mine"
 pubsub_orig_repo = "github.com/libp2p/go-floodsub"
 pubsub_mine_repo = "github.com/mhchia/go-floodsub"
 pubsub_mine_url = f"https://{pubsub_mine_repo}"
 pubsub_branch = "gossipsub-buffersize-changed"
 
-# set connections
-
+# TODO: hacky way to make use of `ThreadingGroup`
 class CustomConnection(Connection):
 
     node_index = None
@@ -57,22 +56,23 @@ class CustomConnection(Connection):
             custom_cmd = custom_kwargs[self.node_index]
             orig_kwargs = dicttoolz.dissoc(kwargs, 'custom_kwargs')
             return super().run(custom_cmd, **orig_kwargs)
-        else:
-            return super().run(*args, **kwargs)
+        return super().run(*args, **kwargs)
 
 
 # specify which host a node locates
 # e.g. hosts = [h0, h1, h2]
 #      node_host_index_map = [h0, h1, h0, h2]
 # (number of nodes) == len(node_host_index_map)
-node_host_index_map = tuple(range(len(hosts)))
+# node_host_index_map = tuple(range(len(hosts)))
+# node_host_index_map = (0, 1, 2, 1, 0, 2, 0, 1, 2)
+node_host_index_map = (0,1,0,1,0,1)
 node_target = {
-    1:0,
-    2:1,
+    i+1: i for i in range(len(node_host_index_map) - 1)
 }
 node_send_collation = {
-    2: (10, 1),
+    (len(node_host_index_map) - 1): (100, 1, 1000),
 }
+
 
 def get_host_conns():
     for node_index, host in enumerate(hosts):
@@ -87,6 +87,13 @@ def get_host_conns():
 
 def get_node_conns():
     for node_index, host_index in enumerate(node_host_index_map):
+        if host_index >= len(hosts):
+            raise ValueError(
+                "host_index {} >= len(hosts)={}".format(
+                    host_index,
+                    len(hosts)
+                )
+            )
         yield CustomConnection(
             user=hosts[host_index].user,
             host=hosts[host_index].ip,
@@ -107,6 +114,9 @@ def make_and_cmd(cmds):
 def make_batch_cmd(cmds):
     return " ; ".join(cmds)
 
+
+# constants
+SCALE = 2
 
 # cmds
 cmd_set_env = make_batch_cmd([
@@ -160,30 +170,30 @@ def update_build_poc(conns):
 
 def run_nodes(conns):
     commands = {}
-    for index, _ in enumerate(conns):
-        program_cmd = f"./minimal -seed {index} "
-        if index in node_target:
-            target_node_index = node_target[index]
+    for node_index, _ in enumerate(conns):
+        program_cmd = f"./minimal -seed {node_index} "
+        if node_index in node_target:
+            target_node_index = node_target[node_index]
             target_node_host_index = node_host_index_map[target_node_index]
             program_cmd += "-target-seed {} -target-ip {} ".format(
                 target_node_index,
                 hosts[target_node_host_index].ip,
             )
-        if index in node_send_collation:
+        if node_index in node_send_collation:
             program_cmd += "-send {} ".format(
                 ",".join(
                     map(
                         str,
-                        node_send_collation[index],
+                        node_send_collation[node_index],
                     )
                 )
             )
         node_cmd = make_and_cmd([
             f"cd {poc_path}",
-            "sleep 1",
+            "sleep {}".format(node_index * SCALE),
             program_cmd,
         ])
-        commands[index] = node_cmd
+        commands[node_index] = node_cmd
     print(conns)
     print(hosts)
     print(commands)
